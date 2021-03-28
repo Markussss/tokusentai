ae94f97497a6eaa12240b50191cb0d1b8fbcdabf
Markus Igeland <markusigeland@gmail.com>
2021-03-28 15:15:58 +0200

    Some word changes

--- bot.js
+++ bot.js
@@ -18,36 +18,36 @@ export async function getClient() {
   return client;
 }
 
-export async function getMessages(channelId, messageId, forward = false, handler = (a) => a) {
+export async function getMessages(channelId, messageId, backward = false, handler = (a) => a) {
   await getClient();
   const channel = await client.channels.fetch(channelId);
 
-  log(`Downloading messages ${forward ? 'before' : 'after'} ${messageId}`);
+  log(`Downloading messages ${backward ? 'before' : 'after'} ${messageId}`);
   let messageHistory;
   let edgeMessage;
   const searchQuery = {
     limit: 100,
   };
   if (messageId) {
-    searchQuery[forward ? 'before' : 'after'] = messageId;
+    searchQuery[backward ? 'before' : 'after'] = messageId;
   }
   while (!messageHistory || messageHistory.length > 0) {
-    log(`Downloading messages ${forward ? 'before' : 'after'} ${searchQuery[forward ? 'before' : 'after']}`);
+    log(`Downloading messages ${backward ? 'before' : 'after'} ${searchQuery[backward ? 'before' : 'after']}`);
     messageHistory = Array.from(
       await channel.messages.fetch(searchQuery), // eslint-disable-line no-await-in-loop
     ).map((message) => message[1]);
     await handler(messageHistory);// eslint-disable-line no-await-in-loop
     if (messageHistory.length === 0) {
-      info(`Finished getting message history ${forward ? 'before' : 'after'}`);
+      info(`Finished getting message history ${backward ? 'before' : 'after'}`);
       return;
     }
 
-    if (forward) {
+    if (backward) {
       edgeMessage = _.minBy(messageHistory, 'id');
     } else {
       edgeMessage = _.maxBy(messageHistory, 'id');
     }
-    searchQuery[forward ? 'before' : 'after'] = edgeMessage.id;
+    searchQuery[backward ? 'before' : 'after'] = edgeMessage.id;
   }
 }
 
